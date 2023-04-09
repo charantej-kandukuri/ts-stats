36a6b354d3530535d6a0ae0f88b60178522c4ab8
charantej-kandukuri <kcharantej@outlook.com>
2023-04-09 19:37:32 +0530

    enum - enumeration

--- build/index.js
+++ build/index.js
@@ -8,6 +8,32 @@ const matches = fs_1.default.readFileSync('football.csv', {
     encoding: 'utf-8'
 }).split('\n')
     .map((row) => {
-    return row.split(', ');
+    return row.split(',');
 });
-console.log('matches', matches);
+let manUnitedWins = 0;
+// A set of possible outcomes
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
+/**
+ * Enum
+ * 1. What are enums and when to use it
+ *
+ * --> Follow near-identical syntax rules as normal objects
+ * --> Creates an object with the same keys and values when converted from TS to JS
+ * --> Primary goal is to signal other engineers that these are all closely related values
+ * --> Use whenever we have a small fixed set of values that are all closely realted and known at compile time
+ */
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,37 @@ const matches = fs.readFileSync('football.csv',{
     encoding: 'utf-8'
 }).split('\n')
 .map((row: string): string[] => {
-    return row.split(', ')
+    return row.split(',')
 })
 
-console.log('matches', matches)  
+let manUnitedWins = 0;
+
+// A set of possible outcomes
+// enum - enumeration
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+}
+
+for (let match of matches) {
+    if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
+
+/**
+ * Enum
+ * 1. What are enums and when to use it
+ * 
+ * --> Follow near-identical syntax rules as normal objects
+ * --> Creates an object with the same keys and values when converted from TS to JS
+ * --> Primary goal is to signal other engineers that these are all closely related values
+ * --> Use whenever we have a small fixed set of values that are all closely realted and known at compile time
+ */
+
+
